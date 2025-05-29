a42c89f860a3ee3507420f4d9136c0c889ede7cf
FSR <fabiourbinasoares@gmail.com>
2025-05-29 19:01:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from RPA_INICIAL import executar_automacao  # importa sua função
+import os  # necessário para pegar a variável de ambiente PORT
 
 app = Flask(__name__)
 
@@ -10,4 +11,4 @@ def executar():
     return jsonify({"resultado": resultado})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
