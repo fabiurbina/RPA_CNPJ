6aea3b6299d1a1841de85dbaf2f60b17449c1915
FSR <fabiourbinasoares@gmail.com>
2025-05-29 19:06:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify
 from RPA_INICIAL import executar_automacao  # importa sua função
-import os  # necessário para pegar a variável de ambiente PORT
+import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'API do RPA CNPJ no ar! Use POST em /executar-rpa.'
 
 @app.route('/executar-rpa', methods=['POST'])
 def executar():
     dados = request.json  # dados enviados pelo front
-    resultado = executar_automacao(dados)  # agora sua função aceita os dados
+    resultado = executar_automacao(dados)
     return jsonify({"resultado": resultado})
 
 if __name__ == '__main__':
